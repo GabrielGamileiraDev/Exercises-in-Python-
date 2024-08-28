8fedc45d0df5856801677611e383ed99a33e2ca7
Biel Dev <gabrielgamileira512@gmail.com>
2024-08-28 10:17:20 -0300

    Create Exercícios.py

---
+++ Exercícios.py
@@ -0,0 +1,30 @@
+
+#05 question
+
+media1 = int(input("Digite sua primeira nota"))
+media2= int(input("Digite sua segunda nota"))
+print ((media1+media2)/2)
+
+#06 question
+
+base = int(input("Digite a medida da base:"))
+altura = int(input("Digite a altura:"))
+print (base * altura/2)
+
+#07 question
+
+numero = int(input("Escolha um respectivo número:"))
+sucessor = int(numero + 1)
+print("O sucessor do seu respectivo número é de:", sucessor)
+
+#08 question
+
+celsius = float(input("Escolha uma temperatura em Celsius:"))
+fahrenheit = print(float(celsius * 9/5) +32)
+
+#09 question
+
+aleatoria = input("Qual produto deseja?")
+produto = float(input("Qual o preço você deseja?"))
+desconto = float(input("Quanto de desconto deseja?"))
+valor_final = print(produto/desconto, ", este é o desconto deste produto.")
